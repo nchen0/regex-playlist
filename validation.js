d17e8b6806c533f6e0eb5e6c76d7b94315814c84
Nicky Chen <nicky.chen0@gmail.com>
2018-06-30 13:43:10 -0700

    form validation

--- validation.js
+++ validation.js
@@ -1,1 +1,28 @@
-// validation script here
+var reg = /[a-z]/gi;
+var reg2 = new RegExp(/[a-z]/, "gi"); // This is the 2nd way to create regex expressions.
+
+//validation script here
+const patterns = {
+  telephone: /^\d{11}$/,
+  username: /^[a-z\d]{5,12}$/i,
+  password: /^.{8,20}$/,
+  slug: /^[a-z\d-]{8,20}$/,
+  email: /^([a-z\d\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/
+};
+
+//Validation function
+function validate(field, regex) {
+  if (regex.test(field.value)) {
+    field.className = "valid";
+  } else {
+    field.className = "invalid";
+  }
+}
+
+//Storing inputs
+const inputs = document.querySelectorAll("input");
+inputs.forEach(input => {
+  input.addEventListener("keyup", e => {
+    validate(e.target, patterns[e.target.name]);
+  });
+});
